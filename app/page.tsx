f4173bbc1c42dbafb68a4269ba2d122b4f370f06
YGN <ygntechstartup@gmail.com>
2025-01-04 19:03:20 +0530

    dynamic fetching

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import { getrepos } from "@/server/github";
 import { Container } from "@/components/ui/container";
 import { Project } from "./constant";
 dotenv.config({ path: '.env.local' });
+export const dynamic = "force-dynamic";
 
 
 
@@ -25,14 +26,6 @@ const Home = async () => {
       <Hero />
       
       <Container className="py-20 space-y-20">
-        {/* <section>
-          <h2 className="text-3xl font-bold mb-8">Recent Blogs</h2>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-            {blogs.map((blog, index) => (
-              <BlogCard key={index} {...blog} />
-            ))}
-          </div>
-        </section> */}
 
         <section>
           <h2 className="text-3xl font-bold mb-8">Latest Projects</h2>
